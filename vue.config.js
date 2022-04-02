809751e9b7f60603ed8e94d23b6954de2db3f8eb
Abrahan <scr44gr@gmail.com>
2022-04-02 17:01:54 -0400

    update: vue.config.js file

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@
 const { ChakraLoaderPlugin } = require('chakra-loader')
 
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'
+		? '././'
+		: './',
 	configureWebpack: {
 		plugins: [
 			new ChakraLoaderPlugin()
